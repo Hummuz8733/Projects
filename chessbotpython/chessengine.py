dbe92bd19d60436b6df62037de912ecbd9c623cc
Hummuz8733 <witekjakub@interia.pl>
2025-01-29 20:56:50 +0100

    nice

--- chessbotpython/chessengine.py
+++ chessbotpython/chessengine.py
@@ -7,9 +7,99 @@ class GameState():
             [ "--", "--", "--", "--", "--", "--", "--", "--" ],
             [ "--", "--", "--", "--", "--", "--", "--", "--" ],
             [ "--", "--", "--", "--", "--", "--", "--", "--" ],
-            [ "--", "--", "--", "--", "--", "--", "--", "--" ],
             [ "wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp" ],
             [ "wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR" ],
         ]
         self.whiteToMove = True
         self.moveLog = []
+
+    def makeMove( self, move ):
+        self.board[ move.startRow ][ move.startColumn ] = "--"
+        self.board[ move.endRow ][ move.endColumn ] = move.pieceMoved
+        self.moveLog.append( move )
+        self.whiteToMove = not self.whiteToMove
+
+    def undoMove( self ):
+        if len( self.moveLog ) != 0:
+            lastMove = self.moveLog.pop()
+            self.board[ lastMove.startRow ][ lastMove.startColumn ] = lastMove.pieceMoved
+            self.board[ lastMove.endRow ][ lastMove.endColumn ] = lastMove.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
+
+
+
+    def getValidMoves( self ):
+        return self.getPossibleMoves()
+
+    def getPossibleMoves( self ):
+        moves = []
+        for i in range( len( self.board ) ):
+            for j in range( len( self.board[ i ] ) ):
+                color = self.board[ i ][ j ][ 0 ]
+
+
+
+
+class Move():
+
+    rankToRows = {
+        "1" : 7, "2" : 6, "3" : 5, "4" : 4,
+        "5" : 3, "6" : 2, "7" : 1, "8" : 0  
+    }
+    rowsToRanks = { v: k for k , v in rankToRows.items() }
+    filesToColumns = {
+        "a" : 0, "b" : 1, "c" : 2, "d" : 3,
+        "e" : 4, "f" : 5, "g" : 6, "h" : 7  
+    }
+    columnsToFiles = { v: k for k , v in filesToColumns.items() }
+    
+
+    def __init__( self, startSq, endSq, board ):
+        self.startRow = startSq[ 0 ]
+        self.startColumn = startSq[ 1 ]
+        self.endRow = endSq[ 0 ]
+        self.endColumn = endSq[ 1 ]
+        self.pieceMoved = board[ self.startRow ][ self.startColumn ]
+        self.pieceCaptured = board[ self.endRow ][ self.endColumn ]
+
+    def getRankFile( self, rank, column ):
+        return self.columnsToFiles[ column ] + self.rowsToRanks[ rank ]
+
+
+    def getChessNotation( self ):
+        return self.getRankFile( self.startRow, self.startColumn) + self.getRankFile( self.endRow, self.endColumn )
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- chessbotpython/chessmain.py
+++ chessbotpython/chessmain.py
@@ -1,5 +1,6 @@
 import pygame as p
 import chessengine
+from chessengine import *
 
 WIDTH = HEIGHT = 512
 DIMENSION = 8
@@ -12,12 +13,65 @@ def loadImages():
     for piece in pieces:
         IMAGES[ piece ] = p.transform.scale( p.image.load( "images/" + piece + ".png" ), ( SQ_SIZE, SQ_SIZE ) )
 
+
+def drawBoard( screen ):
+    colors = [ p.Color( "antiquewhite1" ), p.Color( "burlywood3" ) ]
+    for i in range( DIMENSION ):
+        for j in range( DIMENSION ):
+            color = colors[ ( ( i + j ) % 2 ) ]
+            p.draw.rect( screen, color, p.Rect( j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE ) ) 
+
+def drawPieces( screen, board ):
+    for i in range( DIMENSION ):
+        for j in range( DIMENSION ):
+            piece = board[ i ][ j ]
+            if piece != "--":
+                screen.blit( IMAGES[ piece ], p.Rect( j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE ) )
+
+def drawGameState( screen, gs ):
+    drawBoard( screen )
+    drawPieces( screen, gs.board )
+
 def main():
     p.init()
-    screen = p.display.set_mode( WIDTH, HEIGHT )
+    screen = p.display.set_mode( ( WIDTH, HEIGHT ) )
     clock = p.time.Clock() 
     screen.fill( p.Color( "white" ) )
     gs = chessengine.GameState()
-    print(gs.board)
-    
+    loadImages()
+    running = True
+    sqSelected = ()
+    playerClicks = []
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                column = location[ 0 ] // SQ_SIZE
+                row = location[ 1 ] // SQ_SIZE
+                if sqSelected == ( row, column ):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = ( row, column )
+                    playerClicks.append( sqSelected )
+                if len( playerClicks ) == 2:
+                    move = chessengine.Move( playerClicks[ 0 ], playerClicks[ 1 ], gs.board )
+                    print( move.getChessNotation() )
+                    gs.makeMove( move )
+                    sqSelected = ()
+                    playerClicks = []
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undoMove()
+                
+        drawGameState( screen, gs )
+        clock.tick( MAX_FPS )
+        p.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
 
