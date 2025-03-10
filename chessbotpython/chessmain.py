3761fcb20baca2d5b194731c42b381c8e0449bfc
Hummuz8733 <witekjakub@interia.pl>
2025-03-10 22:17:48 +0100

    move and selection highlighting

--- chessbotpython/chessmain.py
+++ chessbotpython/chessmain.py
@@ -14,12 +14,18 @@ def loadImages():
         IMAGES[ piece ] = p.transform.scale( p.image.load( "GitHub/Projects/chessbotpython/images/" + piece + ".png" ), ( SQ_SIZE, SQ_SIZE ) )
 
 
-def drawBoard( screen,  ):
+def drawBoard( screen, lastMove, sqSelected ):
     colors = [ p.Color( "antiquewhite1" ), p.Color( "burlywood3" ) ]
     colorsHighlighted = [ p.Color( "coral1" ), p.Color( "brown1" ) ]
+    colorsSelected = [ p.Color( "darkolivegreen3" ), p.Color( "darkolivegreen3" ) ]
     for i in range( DIMENSION ):
         for j in range( DIMENSION ):
-            color = colors[ ( ( i + j ) % 2 ) ]
+            if lastMove != () and ( ( i, j ) == lastMove[ 0 ] or ( i, j ) == lastMove[ 1 ] ):
+                color = colorsHighlighted[ ( ( i + j ) % 2 ) ]
+            elif sqSelected != () and ( i, j ) == sqSelected:
+                color = colorsSelected[ ( ( i + j ) % 2 ) ]
+            else:
+                color = colors[ ( ( i + j ) % 2 ) ]
             p.draw.rect( screen, color, p.Rect( j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE ) ) 
 
 def drawPieces( screen, board ):
@@ -29,8 +35,8 @@ def drawPieces( screen, board ):
             if piece != "--":
                 screen.blit( IMAGES[ piece ], p.Rect( j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE ) )
 
-def drawGameState( screen, gs ):
-    drawBoard( screen )
+def drawGameState( screen, gs, lastMove, sqSelected ):
+    drawBoard( screen, lastMove, sqSelected )
     drawPieces( screen, gs.board )
 
 def main():
@@ -45,6 +51,7 @@ def main():
     running = True
     sqSelected = ()
     playerClicks = []
+    lastMove = ()
 
     while running:
         for e in p.event.get():
@@ -57,9 +64,11 @@ def main():
                 if sqSelected == ( row, column ):
                     sqSelected = ()
                     playerClicks = []
+                    drawGameState( screen, gs, lastMove, sqSelected )
                 else:
                     sqSelected = ( row, column )
                     playerClicks.append( sqSelected )
+                    drawGameState( screen, gs, lastMove, sqSelected )
                 if len( playerClicks ) == 2:
                     if ( playerClicks[ 0 ] == ( 7, 4 ) and ( playerClicks [ 1 ] == ( 7, 0 ) or playerClicks[ 1 ] == ( 7, 7 ) ) ) or ( playerClicks[ 0 ] == ( 0, 4 ) and ( playerClicks [ 1 ] == ( 0, 0 ) or playerClicks[ 1 ] == ( 0, 7 ) ) ):
                         move = chessengine.Move( playerClicks[ 0 ], playerClicks[ 1 ], gs.board, False, True )
@@ -69,6 +78,7 @@ def main():
                     for i in range( len( validMoves ) ):
                         if move == validMoves[ i ]:
                             gs.makeMove( validMoves[ i ] )
+                            lastMove = ( playerClicks[ 0 ], playerClicks[ 1 ] )
                             moveMade = True
                             sqSelected = ()
                             playerClicks = []
@@ -79,11 +89,12 @@ def main():
                 if e.key == p.K_z:
                     gs.undoMove()
                     moveMade = True
+                    move = ( ( gs.moveLog[ -1 ].startRow, gs.moveLog[ -1 ].startColumn ), ( gs.moveLog[ -1 ].endRow, gs.moveLog[ -1 ].endColumn ) )
 
         if moveMade:
             validMoves = gs.getValidMoves()
             moveMade = False
-        drawGameState( screen, gs )
+        drawGameState( screen, gs, lastMove, sqSelected )
         clock.tick( MAX_FPS )
         p.display.flip()
 
